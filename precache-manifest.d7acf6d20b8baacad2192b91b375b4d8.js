8cf24794477685c1566605107c689d0cc9f626f5
ngthu1995 <ngthu1995@gmail.com>
2024-06-07 01:23:01 -0400

    Updates

---
+++ precache-manifest.d7acf6d20b8baacad2192b91b375b4d8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "1458718ea91661e94f4b",
+    "url": "/tic-tac-toe-valentine/static/js/main.1458718e.chunk.js"
+  },
+  {
+    "revision": "79dfb14461efeed22306",
+    "url": "/tic-tac-toe-valentine/static/js/1.79dfb144.chunk.js"
+  },
+  {
+    "revision": "ab22ae01268de442d67b",
+    "url": "/tic-tac-toe-valentine/static/js/runtime~main.ab22ae01.js"
+  },
+  {
+    "revision": "29a2a2946c09c3344f5c9a5f182bda54",
+    "url": "/tic-tac-toe-valentine/static/media/heart.29a2a294.png"
+  },
+  {
+    "revision": "7d96fef6757494cd2d1448277a5c0e83",
+    "url": "/tic-tac-toe-valentine/static/media/text_logo.7d96fef6.png"
+  },
+  {
+    "revision": "18d672862f5ea25533043b634b36da25",
+    "url": "/tic-tac-toe-valentine/static/media/js1.18d67286.png"
+  },
+  {
+    "revision": "b8c979af301f86e7cd900680b9235f48",
+    "url": "/tic-tac-toe-valentine/static/media/js2.b8c979af.png"
+  },
+  {
+    "revision": "19624c37370ea8e019441c71c2c4391b",
+    "url": "/tic-tac-toe-valentine/static/media/js3.19624c37.png"
+  },
+  {
+    "revision": "3db9b4c0e8256a948be73642759983db",
+    "url": "/tic-tac-toe-valentine/static/media/js6.3db9b4c0.png"
+  },
+  {
+    "revision": "46863318954b46ca7224cf94efbd772d",
+    "url": "/tic-tac-toe-valentine/static/media/js8.46863318.png"
+  },
+  {
+    "revision": "4e37242c254e12dc454970f097de040d",
+    "url": "/tic-tac-toe-valentine/static/media/js11.4e37242c.png"
+  },
+  {
+    "revision": "0abe812d9be9055c886bdaa9bf2e85d0",
+    "url": "/tic-tac-toe-valentine/static/media/js13.0abe812d.png"
+  },
+  {
+    "revision": "9eabe85fb02379b5dbeb316a2e3b7a6d",
+    "url": "/tic-tac-toe-valentine/static/media/js14.9eabe85f.png"
+  },
+  {
+    "revision": "c0f37741e6625b48332a538ed0da398e",
+    "url": "/tic-tac-toe-valentine/static/media/305_ramac.c0f37741.png"
+  },
+  {
+    "revision": "f648fc66124fecff04064a9cc621c7f6",
+    "url": "/tic-tac-toe-valentine/static/media/strawberries.f648fc66.png"
+  },
+  {
+    "revision": "00e875bea5f5685717332dc8d6d2f04e",
+    "url": "/tic-tac-toe-valentine/static/media/carbonara.00e875be.png"
+  },
+  {
+    "revision": "bb57b08aaef90fa6d6dc3dc381b651b5",
+    "url": "/tic-tac-toe-valentine/static/media/brulee.bb57b08a.png"
+  },
+  {
+    "revision": "269755285047df18c43b1be449995629",
+    "url": "/tic-tac-toe-valentine/static/media/risotto.26975528.png"
+  },
+  {
+    "revision": "92adaa33bff5ad86325d760d822f787d",
+    "url": "/tic-tac-toe-valentine/static/media/cheesecake.92adaa33.png"
+  },
+  {
+    "revision": "af7b26c5a19a0b95434cd834601d0ff7",
+    "url": "/tic-tac-toe-valentine/static/media/mussel.af7b26c5.png"
+  },
+  {
+    "revision": "644bf9bd16dd8e2a08e8417beae8af6d",
+    "url": "/tic-tac-toe-valentine/static/media/steak.644bf9bd.png"
+  },
+  {
+    "revision": "294c6ca2666feda4d8c133d7abb97ada",
+    "url": "/tic-tac-toe-valentine/static/media/cupcake.294c6ca2.png"
+  },
+  {
+    "revision": "e49f00baea8ef171ccc961977360ba66",
+    "url": "/tic-tac-toe-valentine/index.html"
+  }
+];
